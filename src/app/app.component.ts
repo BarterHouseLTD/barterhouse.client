2be292ec50c53927974effe8b4c21d2b34182540
b.vulkov <b.vulkov@adminsoft.bg>
2025-07-17 16:08:35 +0300

    Add FooterComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import {Component} from '@angular/core';
-import {RouterOutlet} from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import { FooterComponent, NavbarComponent } from '@barterhouse/components';
 
 @Component({
   selector: 'bh-root',
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    NavbarComponent,
+    FooterComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'

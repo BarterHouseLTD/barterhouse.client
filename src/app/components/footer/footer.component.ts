642abf4a4f85171793d78899b2cdbe04e4ed9941
b.vulkov <b.vulkov@adminsoft.bg>
2025-07-18 15:49:34 +0300

    Update FooterComponent

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import {Subscription} from 'rxjs';
+import {RouterLink} from '@angular/router';
+import {Component, inject, OnDestroy, OnInit} from '@angular/core';
+
+import {appRoutes} from '@barterhouse/constants';
+import {Culture} from '@barterhouse/enumerations';
+import {CultureService} from '@barterhouse/services/common';
 
 @Component({
   selector: 'bh-footer',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit, OnDestroy {
+  private cultureService: CultureService = inject(CultureService);
+
+  private subs: Subscription = new Subscription();
+
+  protected selectedCulture: Culture = Culture.BG;
+  protected readonly Culture = Culture;
+  protected readonly appRoutes = appRoutes;
+
+  public ngOnInit(): void {
+    this.createCultureSubscription();
+  }
+
+  public ngOnDestroy(): void {
+    this.subs.unsubscribe();
+  }
+
+  private createCultureSubscription(): void {
+    const subscription: Subscription = this.cultureService.getCulture().subscribe({
+      next: culture => {
+        this.selectedCulture = culture;
+      }
+    });
+    this.subs.add(subscription);
+  }
 }

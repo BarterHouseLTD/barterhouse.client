faf9b0eda55de68060cf22878e41b83e30582bc5
Borislav Vulkov <vulkov.borislav@gmail.com>
2025-07-21 09:24:17 +0300

    Work in progress

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'bh-home-page',
-  imports: [],
+  imports: [
+    RouterLink,
+    NgForOf
+  ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
--- src/app/pages/product-details-page/product-details-page.component.ts
+++ src/app/pages/product-details-page/product-details-page.component.ts
@@ -1,11 +1,26 @@
-import {Component} from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {RouterLink} from '@angular/router';
+import {Location} from "@angular/common";
+import {appRoutes} from '@barterhouse/constants';
 
 @Component({
   selector: 'bh-product-details-page',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './product-details-page.component.html',
   styleUrl: './product-details-page.component.scss'
 })
 export class ProductDetailsPageComponent {
+  private location: Location = inject(Location);
 
+  protected onBack(): void {
+    this.location.back();
+  }
+
+  protected getFile(): void {
+
+  }
+
+  protected readonly appRoutes = appRoutes;
 }
